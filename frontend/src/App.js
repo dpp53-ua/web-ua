7d9ef85ac8023cbb7d65bd2eeb63162e23167e92
Sara <snm33@gcloud.ua.es>
2025-04-03 12:40:29 +0200

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { BasicLayout, UserLayout } from './Layouts';
+import { BasicLayout } from './Layouts';
 import { NotFound, Home, Login, Register, Placeholder, Categories } from './Pages';
 import { PrivateRoute } from './Components';
 
